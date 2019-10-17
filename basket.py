41c4883768ebdcd3713500061c1f2aa45d944c32
ziv <zivlahav18@gmail.com>
2019-10-17 12:47:01 +0300

    ver5

--- basket.py
+++ basket.py
@@ -3,7 +3,7 @@ from product import *
 class Basket():
     def __init__(self,list1):
         self.list1 = list1
-        print ("daniel sucks")
+        print ("make new basket again")
     
     def printTotalBasket(self):
         sum1 = 0
